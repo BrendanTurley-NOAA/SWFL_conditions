e35091ccda8b7c922b8e8c81d58ebf6dac12fd85
Brendan Turley <crabtails@gmail.com>
2022-03-02 16:24:59 -0600

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # SWFL_conditions
 Southwest Florida environmental conditions
+
+---
+
+# Current conditions
+
+![alt text](https://github.com/imaginaryfish/SWFL_conditions/figures/hycom_bottom_now.png "Current conditions")
---
+++ R_code/hycom_bottom.R
@@ -0,0 +1,269 @@
+library(fields)
+library(ncdf4)
+library(raster)
+library(rgdal)
+library(scales)
+
+
+setwd("~/Desktop/professional/biblioteca/data")
+bathy <- nc_open('etopo1.nc')
+topo <- ncvar_get(bathy, 'Band1')
+topo_lat <- ncvar_get(bathy, 'lat')
+topo_lon <- ncvar_get(bathy, 'lon')
+nc_close(bathy)
+
+################## geographic scope
+lonbox_e <- -80 ### Florida Bay
+lonbox_w <- -87 ### mouth of Mississippi River
+latbox_n <- 31 ### northern coast
+latbox_s <- 24 ### remove the Keys
+
+ind_lat <- which(topo_lat<latbox_n & topo_lat>latbox_s)
+ind_lon <- which(topo_lon<lonbox_e & topo_lon>lonbox_w)
+
+topo_lat <- topo_lat[ind_lat]
+topo_lon <- topo_lon[ind_lon]
+topo <- topo[ind_lon,ind_lat]
+
+### load map
+# setwd("C:/Users/brendan.turley/Desktop/FL_habs/ne_10m_admin_0_countries")
+# setwd("~/Desktop/professional/biblioteca/data/shapefiles/ne_10m_admin_0_countries")
+# world <- readOGR('ne_10m_admin_0_countries.shp')
+setwd("~/Desktop/professional/biblioteca/data/shapefiles/gshhg-shp-2.3.7/GSHHS_shp/h/")
+world <- readOGR('GSHHS_h_L1.shp')
+world <- crop(world, extent(-87, -79, 24, 31))
+
+### colorpalettes
+### breaks and colors
+temp_col <- colorRampPalette(c(1,'purple','darkorange','gold'))
+sal_col <- colorRampPalette(c('purple4','dodgerblue4','seagreen3','khaki1'))
+uv_col <- colorRampPalette(c('white','thistle1','purple2'))
+lm_neg <- colorRampPalette(c(1,'dodgerblue4','lightskyblue1','white'))
+lm_pos <- colorRampPalette(c('white','mistyrose2','firebrick3'))
+
+################## geogrpahic scope
+lonbox_e <- -80.5 ### Florida Bay
+lonbox_e <- (lonbox_e + 360)
+lonbox_w <- -86 ### mouth of Mississippi River
+lonbox_w <- (lonbox_w + 360)
+latbox_n <- 30.5 ### northern coast
+latbox_s <- 24.5 ### remove the Keys
+
+# https://tds.hycom.org/thredds/catalogs/GLBy0.08/expt_93.0.html
+### bathymetry
+b_url <- 'https://tds.hycom.org/thredds/dodsC/datasets/GLBy0.08/expt_93.0/topo/depth_GLBy0.08_09m11.nc'
+### parameters
+# url <- 'https://tds.hycom.org/thredds/dodsC/GLBy0.08/expt_93.0/ts3z'
+url <- 'https://tds.hycom.org/thredds/dodsC/GLBy0.08/expt_93.0'
+### forecast
+f_url <- 'https://tds.hycom.org/thredds/dodsC/GLBy0.08/expt_93.0/FMRC/GLBy0.08_930_FMRC_best.ncd'
+
+data <- nc_open(url)
+
+time <- ncvar_get(data,'time')
+time2 <- as.Date(time/24,origin='2000-01-01 00:00:00')
+time3 <- as.POSIXct(time*3600,origin='2000-01-01',tz='GMT')
+
+lat <- ncvar_get(data,'lat')
+ind_lat <- which(lat>=latbox_s & lat<=latbox_n)
+lon <- ncvar_get(data,'lon')
+ind_lon <- which(lon>=lonbox_w & lon<=lonbox_e)
+
+z <- ncvar_get(data,'depth')
+
+sal_bot_now <- ncvar_get(data,'salinity_bottom',
+                         start=c(ind_lon[1],ind_lat[1],length(time)),
+                         count=c(length(ind_lon),length(ind_lat),1))
+temp_bot_now <- ncvar_get(data,'water_temp_bottom',
+                          start=c(ind_lon[1],ind_lat[1],length(time)),
+                          count=c(length(ind_lon),length(ind_lat),1))
+
+# u_now <- ncvar_get(data,'water_u',
+#                           start=c(ind_lon[1],ind_lat[1],1,length(time)),
+#                           count=c(length(ind_lon),length(ind_lat),1,1))
+# 
+# v_now <- ncvar_get(data,'water_v',
+#                    start=c(ind_lon[1],ind_lat[1],1,length(time)),
+#                    count=c(length(ind_lon),length(ind_lat),1,1))
+
+u_now <- ncvar_get(data,'water_u_bottom',
+                   start=c(ind_lon[1],ind_lat[1],length(time)),
+                   count=c(length(ind_lon),length(ind_lat),1))
+
+v_now <- ncvar_get(data,'water_v_bottom',
+                   start=c(ind_lon[1],ind_lat[1],length(time)),
+                   count=c(length(ind_lon),length(ind_lat),1))
+
+uv_now <- sqrt(u_now^2 + v_now^2)
+u_nows <- u_now[seq(1,length(ind_lon),2),seq(1,length(ind_lat),2)]
+v_nows <- v_now[seq(1,length(ind_lon),2),seq(1,length(ind_lat),2)]
+uv_now_sub <- uv_now[seq(1,length(ind_lon),2),seq(1,length(ind_lat),2)]
+lons <- lon[ind_lon[seq(1,length(ind_lon),2)]]-360
+lats <- lat[ind_lat[seq(1,length(ind_lat),2)]]
+lonlat <- expand.grid(lons,lats)
+names(lonlat) <- c('lon','lat')
+
+n <- 7
+n <- n*8
+sal_bot <- ncvar_get(data,'salinity_bottom',
+                     start=c(ind_lon[1],ind_lat[1],length(time)-n),
+                     count=c(length(ind_lon),length(ind_lat),1+n))
+temp_bot <- ncvar_get(data,'water_temp_bottom',
+                      start=c(ind_lon[1],ind_lat[1],length(time)-n),
+                      count=c(length(ind_lon),length(ind_lat),1+n))
+
+u_bot <- ncvar_get(data,'water_u_bottom',
+                   start=c(ind_lon[1],ind_lat[1],length(time)-n),
+                   count=c(length(ind_lon),length(ind_lat),1+n))
+u_bot <- apply(u_bot,c(1,2),mean,na.rm=T)
+
+v_bot <- ncvar_get(data,'water_v_bottom',
+                   start=c(ind_lon[1],ind_lat[1],length(time)-n),
+                   count=c(length(ind_lon),length(ind_lat),1+n))
+v_bot <- apply(v_bot,c(1,2),mean,na.rm=T)
+
+uv_bot <- sqrt(u_bot^2 + v_bot^2)
+u_bot <- u_bot[seq(1,length(ind_lon),2),seq(1,length(ind_lat),2)]
+v_bot <- v_bot[seq(1,length(ind_lon),2),seq(1,length(ind_lat),2)]
+uv_bot_sub <- uv_bot[seq(1,length(ind_lon),2),seq(1,length(ind_lat),2)]
+
+nc_close(data)
+
+
+### breaks and colors
+sal_breaks <- pretty(sal_bot_now,n=20)
+sal_cols <- sal_col(length(sal_breaks)-1)
+temp_bot_now[which(temp_bot_now<10)] <- 10
+temp_breaks <- pretty(temp_bot_now,n=20)
+temp_cols <- temp_col(length(temp_breaks)-1)
+uv_breaks <- pretty(uv_now,n=20)
+uv_cols <- uv_col(length(uv_breaks)-1)
+uv_breaks2 <- pretty(uv_bot,n=20)
+uv_cols2 <- uv_col(length(uv_breaks2)-1)
+
+
+### plots
+imagePlot(lon[ind_lon]-360,
+          lat[ind_lat],
+          sal_bot_now,breaks=sal_breaks,col=sal_cols,asp=1)
+plot(world,col='gray70',add=T)
+arrows(lonlat$lon,
+       lonlat$lat,
+       lonlat$lon+as.vector(u_nows),
+       lonlat$lat+as.vector(v_nows),
+       length = .025,
+       col=alpha(1,(as.vector(uv_now_sub)/max(uv_now_sub,na.rm=T))))
+contour(topo_lon,topo_lat,topo,
+        add=T,levels=c(-200,-100,-50,-25,-10),col='gray40')
+
+imagePlot(lon[ind_lon]-360,
+          lat[ind_lat],
+          temp_bot_now,breaks=temp_breaks,col=temp_cols,asp=1)
+plot(world,col='gray70',add=T)
+arrows(lonlat$lon,
+       lonlat$lat,
+       lonlat$lon+as.vector(u_nows),
+       lonlat$lat+as.vector(v_nows),
+       length = .025,
+       col=alpha(1,(as.vector(uv_now_sub)/max(uv_now_sub,na.rm=T))))
+contour(topo_lon,topo_lat,topo,
+        add=T,levels=c(-200,-100,-50,-25,-10),col='gray40')
+
+# imagePlot(lon[ind_lon]-360,
+#           lat[ind_lat],
+#           uv_now,breaks=uv_breaks,col=uv_cols,asp=1)
+# plot(world,col='gray70',add=T)
+# arrows(lonlat$lon,
+#        lonlat$lat,
+#        lonlat$lon+as.vector(u_nows),
+#        lonlat$lat+as.vector(v_nows),
+#        length = .025,
+#        col=alpha(1,(as.vector(uv_now_sub)/max(uv_now_sub,na.rm=T))))
+# contour(topo_lon,topo_lat,topo,add=T,levels=c(-200,-100,-50,-25,-10),col='gray40')
+
+imagePlot(lon[ind_lon]-360,
+          lat[ind_lat],
+          uv_bot,breaks=uv_breaks2,col=uv_cols2,asp=1)
+plot(world,col='gray70',add=T)
+arrows(lonlat$lon,
+       lonlat$lat,
+       lonlat$lon+as.vector(u_bot),
+       lonlat$lat+as.vector(v_bot),
+       length = .025,
+       col=alpha(1,(as.vector(uv_bot_sub)/max(uv_bot_sub,na.rm=T))))
+contour(topo_lon,topo_lat,topo,
+        add=T,levels=c(-200,-100,-50,-25,-10),col='gray40')
+
+
+
+### 7 day linear trend
+sal_lm <- sal_p <- matrix(NA,length(ind_lon),length(ind_lat))
+for(i in 1:length(ind_lon)){
+  for(j in 1:length(ind_lat)){
+    if(all(!is.na(sal_bot[i,j,]))){
+      res <- lm(sal_bot[i,j,]~c(1:dim(sal_bot)[3]))
+      sal_lm[i,j] <- coefficients(summary(res))[2]
+      sal_p[i,j] <- coefficients(summary(res))[8]
+    }
+  }
+}
+plot(sal_lm,sal_p)
+sal_lm[which(sal_lm<(-.05))] <- -.05
+sal_lm[which(sal_lm>.05)] <- .05
+sal_lm2 <- sal_lm
+sal_lm2[which(sal_p>.1)] <- NA
+
+
+temp_lm <- temp_p <- matrix(NA,length(ind_lon),length(ind_lat))
+for(i in 1:length(ind_lon)){
+  for(j in 1:length(ind_lat)){
+    if(all(!is.na(temp_bot[i,j,]))){
+      res <- lm(temp_bot[i,j,]~c(1:dim(temp_bot)[3]))
+      temp_lm[i,j] <- coefficients(summary(res))[2]
+      temp_p[i,j] <- coefficients(summary(res))[8]
+    }
+  }
+}
+plot(temp_lm,temp_p)
+temp_lm[which(temp_lm<(-.05))] <- -.05
+temp_lm[which(temp_lm>.05)] <- .05
+temp_lm2 <- temp_lm
+temp_lm2[which(temp_p>.1)] <- NA
+
+
+### breaks and colors
+sal_lm_brks <- seq(-.05,.05,.005)
+sal_lm_cols <- c(lm_neg(length(which(sal_lm_brks<0))),
+                 lm_pos(length(which(sal_lm_brks>0))))
+
+temp_lm_brks <- seq(-.05,.05,.005)
+temp_lm_cols <- c(lm_neg(length(which(temp_lm_brks<0))),
+                  lm_pos(length(which(temp_lm_brks>0))))
+
+
+### plots
+imagePlot(lon[ind_lon]-360,
+          lat[ind_lat],
+          sal_lm2,
+          asp=1,breaks=sal_lm_brks,col=sal_lm_cols)
+plot(world,col='gray70',add=T)
+arrows(lonlat$lon,
+       lonlat$lat,
+       lonlat$lon+as.vector(u_bot),
+       lonlat$lat+as.vector(v_bot),
+       length = .025,
+       col=alpha(1,(as.vector(uv_bot_sub)/max(uv_bot_sub,na.rm=T))))
+contour(topo_lon,topo_lat,topo,add=T,levels=c(-200,-100,-50,-25,-10),col='gray40')
+
+imagePlot(lon[ind_lon]-360,
+          lat[ind_lat],
+          temp_lm2,
+          asp=1,breaks=temp_lm_brks,col=temp_lm_cols)
+plot(world,col='gray70',add=T)
+arrows(lonlat$lon,
+       lonlat$lat,
+       lonlat$lon+as.vector(u_bot),
+       lonlat$lat+as.vector(v_bot),
+       length = .025,
+       col=alpha(1,(as.vector(uv_bot_sub)/max(uv_bot_sub,na.rm=T))))
+contour(topo_lon,topo_lat,topo,add=T,levels=c(-200,-100,-50,-25,-10),col='gray40')
