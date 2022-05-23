cc50c50304be2854971ef1c1afd54c4bce7d7194
Brendan Turley <crabtails@gmail.com>
2022-05-23 16:48:56 -0500

    oisst

---
+++ R_code/oisst_analysis.R
@@ -0,0 +1,60 @@
+
+library(ncdf4)
+
+temp_col <- colorRampPalette(c('gray20','purple','darkorange','gold'))
+anom_neg <- colorRampPalette(c('dodgerblue4','deepskyblue3','lightskyblue1','gray95'))
+anom_pos <- colorRampPalette(c('gray95','rosybrown1','tomato2','red4'))
+
+################## geogrpahic scope
+lonbox_e <- -75 ### Florida Bay
+lonbox_e <- (lonbox_e + 360)
+lonbox_w <- -99 ### mouth of Mississippi River
+lonbox_w <- (lonbox_w + 360)
+latbox_n <- 31 ### northern coast
+latbox_s <- 17.5 ### remove the Keys
+
+url <- 'https://www.ncei.noaa.gov/thredds/dodsC/OisstBase/NetCDF/V2.1/AVHRR/202205/oisst-avhrr-v02r01.20220522_preliminary.nc'
+data <- nc_open(url)
+
+time <- ncvar_get(data,'time')
+z <- ncvar_get(data,'zlev')
+
+lat <- ncvar_get(data,'lat')
+ind_lat <- which(lat>=latbox_s & lat<=latbox_n)
+ind_lat <- ind_lat-1
+lon <- ncvar_get(data,'lon')
+ind_lon <- which(lon>=lonbox_w & lon<=lonbox_e)
+ind_lon <- ind_lon-1
+
+sst <- ncvar_get(data,'sst',
+                 start=c(ind_lon[1],ind_lat[1],1,1),
+                 count=c(length(ind_lon),length(ind_lat),1,1))
+
+anom <- ncvar_get(data,'anom',
+                  start=c(ind_lon[1],ind_lat[1],1,1),
+                  count=c(length(ind_lon),length(ind_lat),1,1))
+
+nc_close(data)
+
+### breaks and colors
+# surface
+sst_brks <- pretty(sst,n=30)
+sst_cols <- temp_col(length(sst_brks)-1)
+# anom
+# anom_brks <- pretty(anom,30)
+anom_brks <- seq(-2,2,.1)
+anom[which(anom<(-2))] <- -2
+anom[which(anom>2)] <- 2
+
+anom_cols <- c(anom_neg(length(which(anom_brks<0))),
+                 anom_pos(length(which(anom_brks>0))))
+
+image(lon[ind_lon]-360,
+      lat[ind_lat],
+      sst,
+      asp=1,breaks=sst_brks,col=sst_cols)
+
+image(lon[ind_lon]-360,
+      lat[ind_lat],
+      anom,
+      asp=1,breaks=anom_brks,col=anom_cols)
