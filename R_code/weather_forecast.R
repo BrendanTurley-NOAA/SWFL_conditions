98f664c3ec9dea63407b01f2617e1af01ea82189
Brendan Turley <crabtails@gmail.com>
2022-10-31 11:40:53 -0500

    surface pressure

--- R_code/weather_forecast.R
+++ R_code/weather_forecast.R
@@ -72,21 +72,58 @@ par(pin=c(3,3))
 # It automatically adds a legend
 imagePlot(model.grid$x, sort(model.grid$y), model.grid$z[1,1,,],
            xlab = "Longitude", ylab = "Latitude",
-           main = paste("Surface Temperature",
+           main = paste("10m Winds",
                         model.grid$fcst.date),asp=1)
 map('world',add=T)
 
 imagePlot(model.grid$x, sort(model.grid$y), model.grid$z[1,2,,],
           xlab = "Longitude", ylab = "Latitude",
-          main = paste("Surface Temperature",
+          main = paste("10m Winds",
                        model.grid$fcst.date),asp=1)
 map('world',add=T)
 
 imagePlot(model.grid$x, sort(model.grid$y), model.grid$uv,
           xlab = "Longitude", ylab = "Latitude",
-          main = paste("Surface Temperature",
+          main = paste("10m Winds",
                        model.grid$fcst.date),asp=1,
           col=plasma(60),nlevel=59)
+arrows(lon.lat$lon, lon.lat$lat,
+       lon.lat$lon+(model.grid$z[1,1,,]/10), lon.lat$lat+(model.grid$z[1,2,,]/10),
+       length=.02,col='gray80')
+map('world',add=T)
+
+
+
+variable <- "pressfc"
+variable <- 'msletmsl'
+time <- c(0, 0) # Analysis run, index starts at 0
+lon <- c(0, 719) # All 720 longitude points (it's total_points -1)
+lat <- c(0, 360) # All 361 latitude points
+lev <- NULL      # DO NOT include level if variable is non-level type
+### https://www.e-education.psu.edu/meteo810/node/1901
+ensemble <- c(0, 9) #All available ensembles
+
+
+print("getting data...")
+model.data <- DODSGrab(latest.model, latest.model.run,
+                       variable, time, levels=lev, lon, lat)
+# ensemble = ensemble)
+
+# reorder the lat/lon so that the maps work
+model.data$lon<-ifelse(model.data$lon>180,model.data$lon-360,model.data$lon)
+
+# regrid the data
+model.grid2 <- ModelGrid(model.data, c(0.5, 0.5), model.domain = c(-100,-79,31,18) )
+
+# replace out-of-bounds values
+# model.grid2$z <- ifelse (model.grid2$z>99999,NA,model.grid2$z)
+
+
+imagePlot(model.grid$x, sort(model.grid$y), model.grid2$z[1,1,,],
+          xlab = "Longitude", ylab = "Latitude",
+          main = paste("10m Winds",
+                       model.grid$fcst.date),asp=1,
+          col=mako(60),nlevel=59)
 arrows(lon.lat$lon, lon.lat$lat,
        lon.lat$lon+(model.grid$z[1,1,,]/10), lon.lat$lat+(model.grid$z[1,2,,]/10),
        length=.02,col='gray80')
